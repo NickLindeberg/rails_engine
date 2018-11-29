857b60d538e132b1fb2fbe57102f30b4393da868
Nick Lindeberg <lindebergnick@gmail.com>
2018-11-29 09:58:45 -0700

    work on top merchant as well as additional setup

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,9 @@
 class Transaction < ApplicationRecord
   validates_presence_of :credit_card_number,
-                        :credit_card_expiration_date,
                         :result
   belongs_to :invoice
+
+  scope :successful, -> { where(result: "success") }
+
+  default_scope { order(:id)}
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,5 +7,23 @@ describe Merchant, type: :model do
   describe 'Relationships' do
     it { should have_many(:items)}
     it { should have_many(:invoices)}
+    it { should have_many(:invoice_items).through(:invoices)}
+  end
+
+  describe 'class methods' do
+    it 'top_rev_merchants' do
+      merchant_1 = Merchant.create(name: "One")
+      merchant_2 = Merchant.create(name: "Two")
+      merchant_3 = Merchant.create(name: "Three")
+      merchant_4 = Merchant.create(name: "Four")
+      invoice_item_1 = merchant_1.invoice_items.create(quantity: 4, unit_price: 10)
+      invoice_item_2 = merchant_2.invoice_items.create(quantity: 1, unit_price: 20)
+      invoice_item_3 = merchant_3.invoice_items.create(quantity: 1, unit_price: 50)
+      invoice_item_4 = merchant_4.invoice_items.create(quantity: 2, unit_price: 5)
+
+      sorted_merchants = Merchant.top_rev_merchants
+
+      expect(sorted_merchants).to eq([merchant_3, merchant_1, merchant_2])
+    end
   end
 end

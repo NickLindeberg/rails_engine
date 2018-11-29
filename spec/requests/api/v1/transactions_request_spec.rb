85c4625a2187c32ba61338b0924af4e69d2871db
Nick Lindeberg <lindebergnick@gmail.com>
2018-11-29 10:51:04 -0700

    adds basic end points for all model for index and show

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Transactions API' do
-  it 'sends a list of items' do
+  it 'sends a list of transactions' do
     create_list(:transaction, 3)
 
     get '/api/v1/transactions.json'

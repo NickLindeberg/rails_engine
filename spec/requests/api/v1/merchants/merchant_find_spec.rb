77ae61c498532db5a59cca136dbd85a58c571086
Nick Lindeberg <lindebergnick@gmail.com>
2018-11-29 13:13:13 -0700

    finishes find one for merchant

--- spec/requests/api/v1/merchants/merchant_find_spec.rb
+++ spec/requests/api/v1/merchants/merchant_find_spec.rb
@@ -12,7 +12,7 @@ describe 'Merchants API' do
     expect(merchant["id"]).to eq(mid)
   end
 
-  it 'finds merchant by name' do
+  it 'finds one merchant by name' do
     mname = create(:merchant).name
     get "/api/v1/merchants/find?=#{mname}"
 
@@ -21,5 +21,27 @@ describe 'Merchants API' do
     merchant = JSON.parse(response.body)
 
     expect(merchant["name"]).to eq(mname)
+  end
+
+  it 'finds one merchant by created_at' do
+    merc = create(:merchant, created_at: "2012-03-27 14:53:59 UTC")
+    get "/api/v1/merchants/find?=#{merc.created_at}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["created_at"]).to eq("2012-03-27T14:53:59.000Z")
+  end
+
+  it 'finds one merchant by updated_at' do
+    merc = create(:merchant, updated_at: "2012-03-27 14:53:59 UTC")
+    get "/api/v1/merchants/find?=#{merc.updated_at}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["updated_at"]).to eq("2012-03-27T14:53:59.000Z")
   end
 end
